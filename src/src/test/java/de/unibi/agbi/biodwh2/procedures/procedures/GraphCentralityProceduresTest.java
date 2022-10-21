417368634a241a917bc3ce4f6b459ad3c2d3bfdd
Anna Fritzen <fritzenanna101@gmail.com>
2022-10-21 17:21:35 +0200

    added working unit test

--- src/src/test/java/de/unibi/agbi/biodwh2/procedures/procedures/GraphCentralityProceduresTest.java
+++ src/src/test/java/de/unibi/agbi/biodwh2/procedures/procedures/GraphCentralityProceduresTest.java
@@ -5,11 +5,14 @@ import de.unibi.agbi.biodwh2.core.model.graph.Node;
 
 import de.unibi.agbi.biodwh2.procedures.ResultSet;
 import de.unibi.agbi.biodwh2.procedures.model.GraphMode;
+import de.unibi.agbi.biodwh2.procedures.model.IdPair;
+import org.jsoup.select.Evaluator;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
 
 import java.io.IOException;
+import java.util.ArrayList;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -131,6 +134,37 @@ class GraphCentralityProceduresTest {
                                                                               graphConnected.findNode("D"),
                                                                               GraphMode.UNDIRECTED).getRow(0)
                                                                 .getValue(1));
+    }
+
+    @Test
+    void betweennessTest() throws IOException {
+        Graph graph = Graph.createTempGraph();
+        Node node1 = graph.addNode("1");
+        Node node2 = graph.addNode("2");
+        Node node3 = graph.addNode("3");
+        Node node4 = graph.addNode("4");
+        Node node5 = graph.addNode("5");
+        Node node6 = graph.addNode("6");
+        Node node7 = graph.addNode("7");
+        Node node8 = graph.addNode("8");
+        Node node9 = graph.addNode("9");
+        graph.addEdge(node1, node2, "e1-2");
+        graph.addEdge(node1, node3, "e1-3");
+        graph.addEdge(node1, node4, "e1-4");
+        graph.addEdge(node2, node3, "e2-3");
+        graph.addEdge(node3, node4, "e3-4");
+        graph.addEdge(node4, node5, "e4-5");
+        graph.addEdge(node4, node6, "e4-6");
+        graph.addEdge(node5, node6, "e5-6");
+        graph.addEdge(node5, node7, "e5-7");
+        graph.addEdge(node5, node8, "e5-8");
+        graph.addEdge(node6, node7, "e6-7");
+        graph.addEdge(node6, node8, "e6-8");
+        graph.addEdge(node7, node8, "e7-8");
+        graph.addEdge(node7, node9, "e7-9");
+
+        assertEquals(7.0, GraphCentralityProcedures.betweenness(graph, node7.getId(), GraphMode.UNDIRECTED).getRow(0).getValue("betweenness"));
+        assertEquals(15.0, GraphCentralityProcedures.betweenness(graph, node4.getId(), GraphMode.UNDIRECTED).getRow(0).getValue("betweenness"));
     }
 
     @Test
